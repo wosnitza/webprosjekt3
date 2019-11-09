bdd63ede435a7c3e7741dfd41c060c74d10cc58b
eiken1 <46945618+eiken1@users.noreply.github.com>
2019-11-09 15:12:25 +0100

    More work on stats page, added simple callbacks

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import numpy as np
+from data import final_dict, ent_cur_list, ac_list
+
+#list_items = []
 
 app = dash.Dash(__name__)
 
@@ -16,9 +19,27 @@ app.layout = html.Div(id="main", children=[
             html.Div(id="supplier", className="box", children=[
                 html.H1(children=["Supplier stats"]),
                 html.Label(id="supplierInput", children=[
-                    html.P(children=["Input number of suppliers"]),
+                    html.H4(children=["Input number of suppliers"]),
                     dcc.Input(id="supplierNr", type="number",
-                              placeholder="Number of suppliers"),
+                              placeholder="Number of suppliers",
+                              autoFocus=False),
+                    html.Button("Submit", id="supplierBtn"),
+                    html.Div(id="supplierNrOutput", children=[
+
+                    ]),
+                    html.H4(children=['Select your used currencies']),
+                    dcc.Dropdown(id="currencyChooser", options=[
+                                 {'label': k, 'value': v} for k, v in final_dict.items()],
+                                 placeholder="Select a currency..",
+                                 searchable=True),
+                    html.Button("Submit", id="currencyBtn"),
+                    html.Div(id="currencyOutput", children=[
+                        html.Ul(id="currencyList", children=[
+                            # html.Li(item) for item in list_items
+                        ]),
+
+                    ]),
+
 
                 ]),
 
@@ -34,6 +55,26 @@ app.layout = html.Div(id="main", children=[
         ])
     ])
 ])
+
+
+@app.callback(
+    dash.dependencies.Output("supplierNrOutput", "children"),
+    [dash.dependencies.Input("supplierBtn", "n_clicks")],
+    [dash.dependencies.State("supplierNr", "value")]
+)
+def showSupplierNr(n_clicks, value):
+    return "You have {} customers!".format(value)
+
+
+@app.callback(
+    dash.dependencies.Output("currencyList", "children"),
+    [dash.dependencies.Input("currencyBtn", "n_clicks")],
+    [dash.dependencies.State("currencyChooser", "value")]
+)
+def addCurrency(n_clicks, value):
+    return html.Li(children=[value])
+    # return list_items.append(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
