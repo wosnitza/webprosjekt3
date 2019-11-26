88942df9ff2167f9d92ae986d364ad1ba621bb69
eiken1 <46945618+eiken1@users.noreply.github.com>
2019-11-26 20:50:33 +0100

    Ongoing implementation of slider for year

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ app.layout = html.Div(className="main", children=[
     html.Img(id="logo", src="/assets/logo.png"),
     html.Button("Test", id="testButton"),
     html.Div(id="hidden", style={"display": "none"}),
+    html.Div(id="hidden2", style={"display": "none"}),
 
     html.Div(id="tabDiv", children=[
             html.Div(id="parameters", children=[
@@ -100,6 +101,10 @@ app.layout = html.Div(className="main", children=[
                                 },
                              ),
                             html.Div(id="uploadDiv", children=[
+
+                            ]),
+
+                            html.Div(id="sliderDiv", children=[
 
                             ]),
                             
@@ -407,83 +412,151 @@ def createAccountTable(n_clicks, value):
 def fetchMockData(value):
     return createTable(value)
 
-#Idea moved to upload.py under data
+#Original callback for file upload, converting the json to a dataframe
+#and then updating several elements in the layout 
+#with information taken from the dataframe
+#@app.callback([
+    #dash.dependencies.Output("uploadDiv", "children"),
+    #dash.dependencies.Output("revDiv", "children"),
+    #dash.dependencies.Output("procDiv", "children"),
+    #dash.dependencies.Output("netIncomeOutput", "children")
+    #],
+    #[dash.dependencies.Input("upload", "contents")],
+    #[dash.dependencies.State("upload", "filename")]
+
+#)
+#def uploadParse(contents, filename):
+    #if contents is None:
+        #raise PreventUpdate
+    #elif contents is not None:
+        #content_type, content_string = contents.split(',')
+
+        #decoded = base64.b64decode(content_string)
+        #if contents is not None:
+            #if 'csv' in filename:
+                #df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+                #print(df)
+                #df.set_index("Year")
+                #Commenting this one, gives COGS keyerror
+                #df.columns = df.columns.str.strip()
+                #This one gives AttributeError: 'Index' object has no attribute 'lower'
+                #df.columns = df.columns.str.strip().lower()
+                #Commenting this one, gives total revenue nok KeyError
+                #df.columns = df.columns.str.lower()
+                #success_msg = html.P("File upload was succesful!")
+                #current_revenue = df.loc["year","Total Revenue [NOK]"]
+                #cogs = df.loc["year","COGS"]
+                #current_cogs = current_revenue * cogs
+                #netInc = df.loc["year","NetInc"]
+                #current_netInc = current_revenue * netInc
+                #current_revenue = df.ix[4, "Total Revenue [NOK]"]
+                #current_cogs = current_revenue * df.ix[4, "COGS"]
+                #current_netInc = current_revenue * df.ix[4, "NetInc"]
+                #return [success_msg, current_revenue, current_cogs, current_netInc]
+            #elif "json" in filename:
+                #df = pd.read_json(io.StringIO(decoded.decode('utf-8')))
+                #pnl_distr, growth_dict = json.load(io.StringIO(decoded.decode('utf8')))
+                #barName = pnl_distr.pop('Headers')
+                #df = pd.DataFrame(pnl_distr)
+                #print(df)
+                #df = df.T
+                #df.columns = barName
+                #df.reset_index(level=0, inplace=True, col_fill="Year")
+                #df.rename(columns={"index": "Year"}, inplace=True)
+                #print(df)
+                #print(df["Total Revenue [NOK]"])
+
+                #success_msg = html.P("File upload was succesful!")
+                #current_revenue = df.loc[4, "Total Revenue [NOK]"]
+                #current_revenue = df['Total Revenue [NOK]'].iloc[-1]
+                #cogs = df.loc[4,"COGS"]
+                #cogs = df['COGS'].iloc[-1]
+                #current_cogs = current_revenue * cogs
+                #netInc = df.loc[4, "NetInc"]
+                #netInc = df['NetInc'].iloc[-1]
+                #current_netInc = current_revenue * netInc
+
+                #return [success_msg, current_revenue, current_cogs, current_netInc]
+    
+
+            #else:
+                #error_msg = "File must be of type .json!"
+                #filler = "Something"
+                #filler2 = "Something else"
+                #filler3 = "Something more"
+                #return [error_msg, error_msg, error_msg, error_msg]
+                #return [1,2,3,4]
+
+
 @app.callback([
-    dash.dependencies.Output("uploadDiv", "children"),
-    dash.dependencies.Output("revDiv", "children"),
-    dash.dependencies.Output("procDiv", "children"),
-    dash.dependencies.Output("netIncomeOutput", "children")
-    ],
+    dash.dependencies.Output("sliderDiv", "children"),
+    dash.dependencies.Output("hidden2", "children")],
     [dash.dependencies.Input("upload", "contents")],
     [dash.dependencies.State("upload", "filename")]
-
 )
-def uploadParse(contents, filename):
+def uploadSlider(contents, filename):
     if contents is None:
         raise PreventUpdate
     elif contents is not None:
         content_type, content_string = contents.split(',')
 
         decoded = base64.b64decode(content_string)
-        if contents is not None:
-            if 'csv' in filename:
-                df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-                print(df)
-                df.set_index("Year")
-                #Commenting this one, gives COGS keyerror
-                #df.columns = df.columns.str.strip()
-                #This one gives AttributeError: 'Index' object has no attribute 'lower'
-                df.columns = df.columns.str.strip().lower()
-                #Commenting this one, gives total revenue nok KeyError
-                #df.columns = df.columns.str.lower()
-                success_msg = html.P("File upload was succesful!")
-                current_revenue = df.loc["year","Total Revenue [NOK]"]
-                cogs = df.loc["year","COGS"]
-                current_cogs = current_revenue * cogs
-                netInc = df.loc["year","NetInc"]
-                current_netInc = current_revenue * netInc
-                #current_revenue = df.ix[4, "Total Revenue [NOK]"]
-                #current_cogs = current_revenue * df.ix[4, "COGS"]
-                #current_netInc = current_revenue * df.ix[4, "NetInc"]
-                return [success_msg, current_revenue, current_cogs, current_netInc]
-            elif "json" in filename:
-                #df = pd.read_json(io.StringIO(decoded.decode('utf-8')))
-                pnl_distr, growth_dict = json.load(io.StringIO(decoded.decode('utf8')))
-                barName = pnl_distr.pop('Headers')
-                df = pd.DataFrame(pnl_distr)
-                print(df)
-                df = df.T
-                df.columns = barName
-                df.reset_index(level=0, inplace=True, col_fill="Year")
-                df.rename(columns={"index": "Year"}, inplace=True)
-                print(df)
-                print(df["Total Revenue [NOK]"])
+        if "json" in filename:
+            pnl_distr, growth_dict = json.load(io.StringIO(decoded.decode('utf8')))
+            barName = pnl_distr.pop('Headers')
+            df = pd.DataFrame(pnl_distr)
+            df = df.T
+            df.columns = barName
+            df.reset_index(level=0, inplace=True, col_fill="Year")
+            df.rename(columns={"index": "Year"}, inplace=True)
+            #print(df)
+            return [dcc.Slider(
+                id="year_slider",
+                min=int(df['Year'].min()),
+                max=int(df['Year'].max()),
+                value=int(df['Year'].min()),
+                marks={str(year): str(year) for year in df['Year'].unique()},
+                step=None
+            ), df.to_json(date_format="iso", orient="split")]
 
-                success_msg = html.P("File upload was succesful!")
-                current_revenue = df.loc[4, "Total Revenue [NOK]"]
-                cogs = df.loc[4,"COGS"]
-                current_cogs = current_revenue * cogs
-                netInc = df.loc[4, "NetInc"]
-                current_netInc = current_revenue * netInc
+@app.callback([
+    dash.dependencies.Output("revDiv", "children"),
+    dash.dependencies.Output("procDiv", "children"),
+    dash.dependencies.Output("netIncomeOutput", "children")
+    ],
+    [dash.dependencies.Input("year_slider", "value")],
+    [dash.dependencies.State("hidden2", "children")]
+)
 
-                return [success_msg, current_revenue, current_cogs, current_netInc]
-    
+def changeYearInfo(value, hidden_df):
+    #hidden_info = json.loads(hidden_df)
+    #new_df = pd.DataFrame(hidden_info)
+    dff = pd.read_json(hidden_df, orient="split")
+    print(dff)
+    stat_list = []
+    for i, c in dff.iterrows():
+        if i == value:
+            print(i)
+            #success_msg = html.P("File upload was succesful!")
+            #current_revenue = dff.loc[4, "Total Revenue [NOK]"]
+            #current_revenue = dff.loc[str(i), "Total Revenue [NOK]"]
+            current_revenue = dff.loc[i, c["Total Revenue [NOK]"]]
+            #current_revenue = dff['Total Revenue [NOK]'].iloc[i]
+            #cogs = dff.loc[4,"COGS"]
+            #cogs = dff['COGS'].iloc[i]
+            cogs = dff.loc[str(i), "COGS"]
+            current_cogs = current_revenue * cogs
+            #netInc = dff.loc[4, "NetInc"]
+            #netInc = dff['NetInc'].iloc[i]
+            netInc = dff.loc[str(i), "NetInc"]
+            current_netInc = current_revenue * netInc
+            print(current_revenue)
+            print(current_cogs)
+            print(current_netInc)
+            return [current_revenue, current_cogs, current_netInc]
+            #stat_list.append(current_revenue, current_cogs, current_netInc)
 
-            else:
-                error_msg = "File must be of type .json!"
-                #filler = "Something"
-                #filler2 = "Something else"
-                #filler3 = "Something more"
-                return [error_msg, error_msg, error_msg, error_msg]
-                #return [1,2,3,4]
-
-
-
-
-
-
-
-
+    #return stat_list
 
 #Below is the upload file callback function
 #But it is designed for use with JSON
