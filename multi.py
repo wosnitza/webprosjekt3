71c0f32a5549a5e9560c7687d9c1662b5920af57
bjorngyles <bjorbord@stud.ntnu.no>
2019-11-30 16:38:32 +0100

    Merging functions. Added charts from json upload with pnl_distribution list

--- multi.py
+++ multi.py
@@ -1,139 +1,316 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from simulator_utils.invoice_generator import generate_list_of_invoices
+from dash.exceptions import PreventUpdate
+import dash_table
+import json
+import pandas as pd
+import base64
+import io
+import plotly.graph_objs as go
 
-print(dcc.__version__)  # 0.6.0 or above is required
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.config['suppress_callback_exceptions'] = True
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app.layout = dbc.Container(
+    dbc.Tabs([
+        # Tab for parameters - Should update other tabs with tables and charts
+        dbc.Tab(id='Tab one', label="Parameters", children=[
+            dbc.Row([
+                dbc.Col(id="file-upload", children=[
+                    dcc.Upload(
+                        id='upload',
+                        children=html.Div([
+                            'Drag and Drop or ',
+                            html.A('Select Files')
+                        ]),
+                        style={
+                            'width': '100%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                            'margin': '10px'
+                        },
+                    )
+                ])
+            ]),
+            dbc.Row([
+                dbc.Col(id="supplier", children=[
+                    html.H1("Supplier stats"),
+                    dbc.Label("Monthly invoices"),
+                    dbc.Input(id="invoice-monthly", className="inputs", type="number", placeholder="Monthly invoices"),
+                    dbc.Label("Invoice year"),
+                    dcc.Dropdown(id="invoice-year",
+                                 options=[
+                                     {'label': '2014', 'value': '2014'},
+                                     {'label': '2015', 'value': '2015'},
+                                     {'label': '2016', 'value': '2016'},
+                                     {'label': '2017', 'value': '2017'},
+                                     {'label': '2018', 'value': '2018'}
+                                 ],
+                                 placeholder="Select Year"
+                                 ),
+                    dbc.Label("Invoices Total cost"),
+                    dbc.Input(id="invoice-amount", type="number", placeholder="Invoice Total Amount"),
+                    html.Div(id="currency-container", children=[
+                        dbc.Label("Select Currencies"),
+                        dcc.Dropdown(id="currency-selector",
+                                     options=[
+                                         {'label': 'Norwegian Krone — NOK', 'value': 'NOK'},
+                                         {'label': 'American Dollars — USD', 'value': 'USD'},
+                                         {'label': 'European Euro', 'value': 'EUR'}
+                                     ]),
+                        html.Div(id="currency-percentage", children=[
+                        ]),
+                    ]),
+                    html.Label("Total procurement"),
+                    dbc.Input(id="total-procurement", type="number",
+                              placeholder="Select total procurement"),
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+                    dbc.Button("Generate", id="chart-generator"),
+                ]),
+                dbc.Col(id="company", children=[
+                    html.H1("Company stats"),
+                    dbc.Label("Accounts"),
+                    dbc.Input(id="companyAccounts", type="number",
+                              placeholder="Number of accounts",
+                              autoFocus=False),
+                    html.Div(id="companyTable", children=[
 
-# Since we're adding callbacks to elements that don't exist in the app.layout,
-# Dash will raise an exception to warn us that we might be
-# doing something wrong.
-# In this case, we're adding the elements through a callback, so we can ignore
-# the exception.
-app.config.suppress_callback_exceptions = True
+                    ]),
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
+                    dbc.Label("Net income"),
+                    dbc.Input(id="netIncomeInput", type="number",
+                              placeholder="Net income"),
+                    html.Div(id="netIncomeOutput", children=[
 
+                    ]),
 
-index_page = html.Div([
-    dcc.Link('Show FX lab', href='/page-1'),
-    html.Br(),
-    dcc.Link('Go to Page 2', href='/page-2'),
-])
+                    dbc.Label("Organization ID"),
+                    dbc.Input(id="orgIdInput", type="number",
+                              placeholder="Organization ID"),
+                    html.Div(id="orgIdOutput", children=[
 
-page_1_layout = html.Div([
-    html.H1('FX LAB'),
-
-
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'NOK', 'value': 'NOK'},
-            {'label': 'USD', 'value': 'USD'},
-            {'label': 'DKK', 'value': 'DKK'}
-        ],
-        value='USD'
-    ),
-
-    dcc.Graph(
-        id='graph',
-        config={
-            'showSendToCloud': True,
-            'plotlyServerURL': 'https://plot.ly'
-        }
-    ),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5],
-                    'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-
-
+                    ]),
+                    html.Div(id="sliderDiv"),
+                ]),
+                dbc.Col(id="customer", children=[
+                    html.H1("Customer stats"),
+                    dbc.Label("Input number of customers"),
+                    dbc.Input(id="customerNr", type="number",
+                              placeholder="Number of customers"),
+                    html.Div(id="customerNrOutput", children=[
+                    ]),
+                    dbc.Label('Select your used currencies'),
+                    dcc.Dropdown(id="currency-selector-customer",
+                                 options=[
+                                     {'label': 'Norwegian Krone — NOK', 'value': 'NOK'},
+                                     {'label': 'American Dollars — USD', 'value': 'USD'},
+                                     {'label': 'European Euro', 'value': 'EUR'}
+                                 ]),
+                    html.Div(id="currency-percentage-customer"),
+                    dbc.Label("Revenue"),
+                    dbc.Input(id="revenueInput", type="number",
+                              placeholder="Select total revenue"),
+                ])
+            ])
+        ]),
+        # Tab for tables or charts
+        dbc.Tab(id="tab-two", label='Invoice table', children=[
+            dash_table.DataTable(id='table'),
+        ]),
+        dbc.Tab(id="tab-three", label="Tab three"),
+        html.Div(id="hidden2", style={"display": "none"})
+    ])
+)
 
 
 
+# Callback for currency selector
+@app.callback(
+    Output("currency-percentage", "children"),
+    [Input("currency-selector", "value")],
+    [State('currency-percentage', 'children')]
+)
+def currencyPercentage(value, existingchildren):
+    if value is None:
+        raise PreventUpdate
+    else:
+        currency = existingchildren + [html.P(value), dcc.Input(id=str("percentage" + value), placeholder="%")]
+        return currency
 
 
-    html.Div(id='page-1-content'),
-    html.Br(),
-    dcc.Link('Go to Page 2', href='/page-2'),
-    html.Br(),
-    dcc.Link('Go back to home', href='/'),
-])
+# Callback for invoice generator. Outputs tables in tabs
+@app.callback(
+    [Output("table", "columns"), Output("table", "data")],
+    [Input("chart-generator", "n_clicks")],
+    [State("invoice-monthly", "value"),
+     State("invoice-year", "value"),
+     State("invoice-amount", "value"),
+     State("currency-percentage", "children")],
+)
+def createCharts(clicks, monthly, year, amount, currencylist):
+    if clicks is None:
+        raise PreventUpdate
+    else:
+        currencycodes = currencylist[::2]
+        currpercentages = currencylist[1::2]
 
-@app.callback(dash.dependencies.Output('page-1-content', 'children'),
-              [dash.dependencies.Input('page-1-dropdown', 'value')])
-def page_1_dropdown(value):
-    return 'You have selected "{}"'.format(value)
+        currencycodeparsed = []
+        for i, j in enumerate(currencycodes):
+            currencycodeparsed.append(currencycodes[i]["props"]["children"])
+
+        currencypercparsed = []
+        for i, j in enumerate(currpercentages):
+            currencypercparsed.append(int(currpercentages[i]["props"]["value"]))
+
+        currency_final = dict(zip(currencycodeparsed, currencypercparsed))
+
+        df = generate_list_of_invoices(int(year), int(monthly), int(amount), currency_final)
+        return [{"name": i, "id": i} for i in df.columns], df.to_dict('records')
+
+
+# Callback for upload and year slider.
+@app.callback([
+    dash.dependencies.Output("sliderDiv", "children"),
+    dash.dependencies.Output("hidden2", "children")],
+    [dash.dependencies.Input("upload", "contents")],
+    [dash.dependencies.State("upload", "filename")]
+)
+def uploadSlider(contents, filename):
+    if contents is None:
+        raise PreventUpdate
+    elif contents is not None:
+        content_type, content_string = contents.split(',')
+
+        decoded = base64.b64decode(content_string)
+        if "json" in filename:
+            pnl_distribution, growth_dict = json.load(io.StringIO(decoded.decode('utf8')))
+            barName = pnl_distribution.pop("Headers")
+
+            df = pd.DataFrame(pnl_distribution)
+
+            for key, value in df.items():
+                value[1:] = value[1:] * value[0]
+
+            df = df.T
+            df.columns = barName
+            df.reset_index(level=0, inplace=True, col_fill="Year")
+            df.rename(columns={"index": "Year"}, inplace=True)
+            # print(df)
+            return [dcc.Slider(
+                id="year_slider",
+                min=int(df['Year'].min()),
+                max=int(df['Year'].max()),
+                value=int(df['Year'].min()),
+                marks={str(year): str(year) for year in df['Year'].unique()},
+                step=None
+            ), df.to_json(date_format="iso", orient="split")]
+
+
+# Callback for uploading json files with proff.no data. Creates a year slider.
+@app.callback([
+    dash.dependencies.Output("revenueInput", "value"),
+    dash.dependencies.Output("total-procurement", "value"),
+    dash.dependencies.Output("netIncomeInput", "value")
+],
+    [dash.dependencies.Input("year_slider", "value")],
+    [dash.dependencies.State("hidden2", "children")]
+)
+def changeYearInfo(value, hidden_df):
+    if not hidden_df:
+        raise PreventUpdate
+    if hidden_df:
+        dff = pd.read_json(hidden_df, orient="split")
+        for i in dff.index:
+            if dff.loc[i, 'Year'] == value:
+                print(i)
+                current_revenue = dff.loc[i, "Total Revenue [NOK]"]
+                current_cogs = dff.loc[i, "COGS"]
+                current_netInc = dff.loc[i, "NetInc"]
+                return [int(current_revenue), int(current_cogs), int(current_netInc)]
 
 
 @app.callback(
-    dash.dependencies.Output('graph', 'figure'),
-    [dash.dependencies.Input('my-dropdown', 'value')])
-def update_output(value):
-    y_array_dict = {
-        'NOK': [4, 2, 3, 1, 2, 1, 4],
-        'USD': [1, 2, 4, 1, 2, 4, 2],
-        'DKK': [5, 3, 6, 5, 3, 6, 2]
-    }
-    return {
-        'data': [{
-            'type': 'scatter',
-            'y': y_array_dict[value]
-        }],
-        'layout': {
-            'title': value
-        }
-    }
-
-
-page_2_layout = html.Div([
-    html.H1('Page 2'),
-    dcc.RadioItems(
-        id='page-2-radios',
-        options=[{'label': i, 'value': i} for i in ['Orange', 'Blue', 'Red']],
-        value='Orange'
-    ),
-    html.Div(id='page-2-content'),
-    html.Br(),
-    dcc.Link('Go to Page 1', href='/page-1'),
-    html.Br(),
-    dcc.Link('Go back to home', href='/')
-])
-
-@app.callback(dash.dependencies.Output('page-2-content', 'children'),
-              [dash.dependencies.Input('page-2-radios', 'value')])
-def page_2_radios(value):
-    return 'You have selected "{}"'.format(value)
-
-
-# Update the index
-@app.callback(dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/page-1':
-        return page_1_layout
-    elif pathname == '/page-2':
-        return page_2_layout
+    Output("tab-three", "children"),
+    [Input("hidden2", "children")]
+)
+def createDistributionChart(hidden_df):
+    if not hidden_df:
+        raise PreventUpdate
     else:
-        return index_page
-    # You could also return a 404 "URL not found" page here
+        df = pd.read_json(hidden_df, orient="split")
 
+        print(df)
+        return [
+            dcc.Graph(figure=go.Figure(
+                data=[
+                    go.Bar(
+                        x=df["Year"],
+                        y=df["Total Revenue [NOK]"],
+                        name='Total Revenue',
+                        marker=go.bar.Marker(
+                            color='rgb(55, 83, 109)'
+                        )
+                    ),
+                    go.Bar(
+                        x=df["Year"],
+                        y=df["COGS"],
+                        name='Cost Of Goods Sold',
+                        marker=go.bar.Marker(
+                            color='rgb(26, 118, 255)'
+                        )
+                    ),
+                    go.Bar(
+                        x=df["Year"],
+                        y=df["SG&A"],
+                        name='Selling, General & Administrative Expense',
+                        marker=go.bar.Marker(
+                            color='rgb(56, 200, 155)'
+                        )
+                    ),
+                    go.Bar(
+                        x=df["Year"],
+                        y=df["DA"],
+                        name='Depreciation Allowance',
+                        marker=go.bar.Marker(
+                            color='rgb(155, 100, 50)'
+                        )
+                    ),
+                    go.Bar(
+                        x=df["Year"],
+                        y=df["IntTax"],
+                        name='IntTax',
+                        marker=go.bar.Marker(
+                            color='rgb(255, 100, 50)'
+                        )
+                    ),
+                    go.Bar(
+                        x=df["Year"],
+                        y=df["NetInc"],
+                        name='Net Income',
+                        marker=go.bar.Marker(
+                            color='rgb(135, 230, 100)'
+                        )
+                    )
+                ],
+                layout=go.Layout(
+                    title='Company Accounting',
+                    showlegend=True,
+                    legend=go.layout.Legend(
+                        x=0,
+                        y=1.0
+                    ),
+                    margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+                )
+            ), id="distGraph"),
+        ]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
